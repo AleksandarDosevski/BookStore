9b8606c24b2c6bd3455e7e9bacabb7b40c3d40ef
Your NameAleksandarDosevski <dosevski@yahoo.com>
2020-12-11 13:32:39 +0100

    Book Store

---
+++ BookStore/Migrations/20200512094612_mig1.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookStore.Migrations
+{
+    public partial class mig1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b4280b6a-0613-4cbd-a9e6-f1701e926e73",
+                column: "ConcurrencyStamp",
+                value: "a19b14da-f28b-43af-a2b8-8d092177d500");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b4280b6a-0613-4cbd-a9e6-f1701e926e74",
+                column: "ConcurrencyStamp",
+                value: "b6abce0f-22c8-415a-af45-e398adc51bf3");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b4280b6a-0613-4cbd-a9e6-f1701e926e75",
+                column: "ConcurrencyStamp",
+                value: "f1017ed6-f548-4957-8246-9bd06834a190");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "b4280b6a-0613-4cbd-a9e6-f1701e926e73",
+                column: "PasswordHash",
+                value: "AQAAAAEAACcQAAAAEBF2g/HrvkXuURcy+BeoPaxMARGbb9huRdC6Vb0PnHAZR7iaFvF4uEQxBCmXjfP8EA==");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b4280b6a-0613-4cbd-a9e6-f1701e926e73",
+                column: "ConcurrencyStamp",
+                value: "b0dd5c8a-6e4d-4085-b9bc-9336d48b6c87");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b4280b6a-0613-4cbd-a9e6-f1701e926e74",
+                column: "ConcurrencyStamp",
+                value: "09ca2fa0-8b70-4c40-bc70-1622c0a85949");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b4280b6a-0613-4cbd-a9e6-f1701e926e75",
+                column: "ConcurrencyStamp",
+                value: "8c282581-97f1-4622-9b96-43bfd19a4079");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "b4280b6a-0613-4cbd-a9e6-f1701e926e73",
+                column: "PasswordHash",
+                value: "AQAAAAEAACcQAAAAEEk28iIgWjg3k9xPFwqg/Gwgt4lLUQvc/QPJmha6mBKQw/5G1Z1P/o7hUgOlURNwhQ==");
+        }
+    }
+}
---
+++ BookStore/Migrations/20200518170743_mig2.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookStore.Migrations
+{
+    public partial class mig2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b4280b6a-0613-4cbd-a9e6-f1701e926e73",
+                column: "ConcurrencyStamp",
+                value: "fc55c879-9f0b-4251-9a59-ec46ade4df44");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b4280b6a-0613-4cbd-a9e6-f1701e926e74",
+                column: "ConcurrencyStamp",
+                value: "0d336dab-0a53-4503-8469-46b0317e79cb");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b4280b6a-0613-4cbd-a9e6-f1701e926e75",
+                column: "ConcurrencyStamp",
+                value: "9f0eccd1-1748-49c7-8754-6277fd091f46");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "b4280b6a-0613-4cbd-a9e6-f1701e926e73",
+                column: "PasswordHash",
+                value: "AQAAAAEAACcQAAAAEGU6pXbG+Tqu+YOkC790XjfP5ubhy3lO8Qy7d/WndSFHVdXQvpPc38M40UdX6Gmmlg==");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b4280b6a-0613-4cbd-a9e6-f1701e926e73",
+                column: "ConcurrencyStamp",
+                value: "a19b14da-f28b-43af-a2b8-8d092177d500");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b4280b6a-0613-4cbd-a9e6-f1701e926e74",
+                column: "ConcurrencyStamp",
+                value: "b6abce0f-22c8-415a-af45-e398adc51bf3");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b4280b6a-0613-4cbd-a9e6-f1701e926e75",
+                column: "ConcurrencyStamp",
+                value: "f1017ed6-f548-4957-8246-9bd06834a190");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "b4280b6a-0613-4cbd-a9e6-f1701e926e73",
+                column: "PasswordHash",
+                value: "AQAAAAEAACcQAAAAEBF2g/HrvkXuURcy+BeoPaxMARGbb9huRdC6Vb0PnHAZR7iaFvF4uEQxBCmXjfP8EA==");
+        }
+    }
+}
